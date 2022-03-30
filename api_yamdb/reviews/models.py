779a26221384fe6ab103f47bb44904c1de0d4a70
Ilya Dudnikov <ilya.d.1991@gmail.com>
2022-03-30 19:19:00 +0300

    Добавлена модель Group

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,3 +5,8 @@ from django.utils.translation import gettext_lazy as _
 class Category(models.Model):
     name = models.CharField(_('Имя категории'), max_length=256)
     slug = models.SlugField(max_length=50)
+
+
+class Genre(models.Model):
+    name = models.CharField('Наименование жанра', max_length=256)
+    slug = models.SlugField('Slug жанра', unique=True)
