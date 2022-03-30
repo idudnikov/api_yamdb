6ba18aa56dfc8be5feb3d3ce4901bb4ba6e6333b
deorz <TheDamnation1044@yandex.ru>
2022-03-30 19:19:02 +0300

    Добавлен str метод модели Category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class Category(models.Model):
     name = models.CharField(_('Имя категории'), max_length=256)
     slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.slug
