d212db679516333d165977d20753665f01548782
deorz <TheDamnation1044@yandex.ru>
2022-03-31 18:05:43 +0300

    Создана модель Title GenreTitle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -13,3 +13,27 @@ class Category(models.Model):
 class Genre(models.Model):
     name = models.CharField('Наименование жанра', max_length=256)
     slug = models.SlugField('Slug жанра', unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(_('Название'), max_length=200, blank=False)
+    year = models.DateTimeField(_('Год выпуска'), auto_now_add=True)
+    rating = models.IntegerField(_('Рейтинг'), null=True)
+    description = models.CharField(_('Описание'), max_length=200)
+    genre = models.ManyToManyField(Genre,
+                                   through='GenreTitle')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.DO_NOTHING
+    )
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.DO_NOTHING
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.DO_NOTHING
+    )
