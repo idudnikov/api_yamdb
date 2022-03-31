ce50d399ac0a44099d0b947e4478f0aa71ba6739
AndreyVnk <94139874+AndreyVnk@users.noreply.github.com>
2022-03-31 16:02:11 +0300

    add urls.py, api/urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .tokens import TokenViewAPI
-from .emailconfirmation import CustomUserViewSet
+from .tokens import TokenVeiwAPI
+from .emailconfirmation import EmailConfirmationViewSet
 
 app_name = "api"
 
 urlpatterns = [
-    path('/api/v1/auth/token/', TokenViewAPI.as_view(), name='token_obtain_pair'),
-    path('/api/v1/auth/signup/', CustomUserViewSet, name = 'register_user'),
+    path('v1/auth/token/', TokenVeiwAPI.as_view(), name='token_obtain_pair'),
+    path('v1/auth/signup/', EmailConfirmationViewSet, name = 'register_user'),
     #path('/api/v1/users/me/', ),
 ]
