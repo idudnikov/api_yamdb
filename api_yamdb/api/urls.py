7d855588d903a915e6f6276a3ef17b7d5ccbe0f3
deorz <TheDamnation1044@yandex.ru>
2022-03-31 18:57:46 +0300

    Незначительные изменения

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,7 +5,8 @@ from .emailconfirmation import CustomUserViewSet
 app_name = "api"
 
 urlpatterns = [
-    path('/api/v1/auth/token/', TokenViewAPI.as_view(), name='token_obtain_pair'),
-    path('/api/v1/auth/signup/', CustomUserViewSet, name = 'register_user'),
-    #path('/api/v1/users/me/', ),
+    path('/api/v1/auth/token/', TokenViewAPI.as_view(),
+         name='token_obtain_pair'),
+    path('/api/v1/auth/signup/', CustomUserViewSet, name='register_user'),
+    # path('/api/v1/users/me/', ),
 ]
